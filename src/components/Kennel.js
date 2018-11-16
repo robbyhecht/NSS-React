d9a2663fc07f155637a32930207c510ecf5a1a7f
robbyhecht <robbyhecht@gmail.com>
2018-11-16 13:29:24 -0600

    initial setup up to practice

---
+++ src/components/Kennel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import EmployeeList from "./employee/EmployeeList"  // Import EmployeeList component
+
+
+export default class Kennel extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Student Kennels</h3>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+        <EmployeeList />
+      </div>
+    );
+  }
+}
