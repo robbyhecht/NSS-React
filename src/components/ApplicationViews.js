94bc44906c0c3651ff3ac898f1675d18d1eff7bd
robbyhecht <robbyhecht@gmail.com>
2018-11-22 11:13:48 -0500

    working through ch5

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -64,6 +64,18 @@ componentDidMount() {
     })
   )
   }
+  deleteEmployee = id => {
+    return fetch(`http://localhost:5002/employees/${id}`, {
+        method: "DELETE"
+    })
+    .then(e => e.json())
+    .then(() => fetch(`http://localhost:5002/employees`))
+    .then(e => e.json())
+    .then(employees => this.setState({
+        employees: employees
+    })
+  )
+  }
 }
 
 
